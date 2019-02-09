b6197175cfbd1e92eee56718e9ed67412e860c6f
Will Miller <46572166+Ceranth@users.noreply.github.com>
2019-02-08 22:19:06 -0600

    Revert "Added green login button"
    This reverts commit 2a4a74117b9c7359da8adc78fd603d5a33c45b93.

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -10,30 +10,12 @@ import { Title } from '../components/display';
 import { ROOT } from '../constants';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import green from '@material-ui/core/colors/green';
-import grey from '@material-ui/core/colors/grey';
-
 
 const GoogleURL = ROOT + "/users/google";
 const FacebookURL = ROOT + "/users/facebook";
 const LoginURL = ROOT + "/users/_/login";
 const AuthURL = ROOT + "/auth";
 
-const buttonStyle = build => ({
-    margin: {
-        margin: 0,
-    },
-    cssRoot: {
-        color: grey[50],
-        backgroundColor: green[500],
-        '&:hover': { 
-            backgroundColor: green[700],
-        },
-    },
-});
 class LoginForm extends Component {
     constructor(props) {
         super(props);
@@ -47,7 +29,6 @@ class LoginForm extends Component {
         this.onTextChange = this.onTextChange.bind(this);
         this.renderLoading = this.renderLoading.bind(this);
     }
-
 
     componentDidMount() {
         axios.get(AuthURL, {withCredentials: true}).then((response)=>{
@@ -106,43 +87,42 @@ class LoginForm extends Component {
     }
 
     render() {
-        const { classes } = this.props;
         return(
-                         <div style={ styles.container }>
+            <div style={ styles.container }>
                 <div style={ styles.outline }>
-                {this.renderRedirect()}
+                    {this.renderRedirect()}
                     <Grid container spacing={8}>
                         <Grid item xs={12}>
                             <Title>SimonSays</Title>
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
-                    required
-                    id="username"
-                    label="Username"
-                    placeholder="Username"
-                    variant="filled"
-                    fullWidth={true}
-                    error={this.state.usernameError}
-                    onChange={this.onTextChange}
-                    disabled={this.state.allDisable}
-                />
-            </Grid>
-            <Grid item xs={12}>
-                <TextField
-                    required
-                    id="password"
-                    label="Password"
-                    placeholder="Password"
-                    variant="filled"
-                    type="password"
-                    fullWidth={true}
-                    error={this.state.passwordError}
-                    onChange={this.onTextChange}
-                    disabled={this.state.allDisable}
-                />
-            </Grid>
-                    {/* <Grid item xs={6}>
+                                required
+                                id="username"
+                                label="Username"
+                                placeholder="Username"
+                                variant="filled"
+                                fullWidth={true}
+                                error={this.state.usernameError}
+                                onChange={this.onTextChange}
+                                disabled={this.state.allDisable}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="password"
+                                label="Password"
+                                placeholder="Password"
+                                variant="filled"
+                                type="password"
+                                fullWidth={true}
+                                error={this.state.passwordError}
+                                onChange={this.onTextChange}
+                                disabled={this.state.allDisable}
+                            />
+                        </Grid>
+                        {/* <Grid item xs={6}>
                             <Fab
                                 variant="extended"
                                 color="primary"
@@ -164,38 +144,37 @@ class LoginForm extends Component {
                             Sign in with Facebook
                             </Fab>
                         </Grid> */}
-                    <Grid item xs={12}>
-                        <Grid container spacing={16}>
-                            <Grid item xl={2}>
-                                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={this.loginManual}
-                    disabled={this.state.allDisable}
-                    className={classNames(classes.margin, classes.cssRoot)}
-                >
-                    Login
-                </Button>
-            </Grid>
-            <Grid item xs={2}>
-                <Button
-                    variant="outlined"
-                    color="primary"
-                    component={Link}
-                    to="/register"
-                    disabled={this.state.allDisable}
-                >
-                    Register
-                </Button>
-            </Grid>
-            <Grid item xs={9} >
-                {this.renderLoading()}
-            </Grid>
-        </Grid>
-    </Grid>
-</Grid>
-</div>
-</div>
+                        <Grid item xs={12}>
+                            <Grid container spacing={8}>
+                                <Grid item xs={2}>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={this.loginManual}
+                                        disabled={this.state.allDisable}
+                                    >
+                                        Login
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Button
+                                        variant="outlined"
+                                        color="primary"
+                                        component={Link}
+                                        to="/register"
+                                        disabled={this.state.allDisable}
+                                    >
+                                        Register
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={9} >
+                                    {this.renderLoading()}
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
         );
     }
 }
@@ -225,4 +204,4 @@ const styles = {
     }
 }
 
-export default withStyles(buttonStyle)(LoginForm);
+export default LoginForm;
